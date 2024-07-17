56bef5e7238c8aef2506d362ac271d08ffed5403
BrK08 <126277787+BrK08@users.noreply.github.com>
2024-07-17 18:45:09 +0300

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -30,18 +30,18 @@ module.exports = {
     try {
 
       const supportServerLink = "Şuanda Discord Serveri bakımda";
-      const githubLink = "geçersiz";
-      const replitLink = "Geçersiz";
-      const youtubeLink = "Reklam İçin mr_berk.";
+      const githubLink = "https://cdn.discordapp.com/attachments/1187105491835429035/1263158485332394065/standard_9.gif?ex=66993783&is=6697e603&hm=ce79a11322a835623a8224376b7df913286e002a940755fee8347a987672883d&";
+      const replitLink = "https://cdn.discordapp.com/attachments/1187105491835429035/1263158485332394065/standard_9.gif?ex=66993783&is=6697e603&hm=ce79a11322a835623a8224376b7df913286e002a940755fee8347a987672883d&";
+      const youtubeLink = "https://cdn.discordapp.com/attachments/1175120971062054964/1216389879093792939/standard_5.gif?ex=6699245a&is=6697d2da&hm=ec71d8276e7295c8b77bc9176c4742ee09a974f1706fce65dcfb58c2f169becc&";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
               name: 'Support Server',
               iconURL: 'https://cdn.discordapp.com/attachments/1175120971062054964/1216388828411990066/standard_2.gif?ex=66992360&is=6697d1e0&hm=788908c77c87315ff6c1e70670d6b6ad098a00f39924ae9487e78e47fcc0f4e8&', 
-              url: 'discord server şuanda bakımda'
+              url: 'https://cdn.discordapp.com/attachments/1175120971062054964/1216389879093792939/standard_5.gif?ex=6699245a&is=6697d2da&hm=ec71d8276e7295c8b77bc9176c4742ee09a974f1706fce65dcfb58c2f169becc&'
           })
             .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setImage('https://cdn.discordapp.com/attachments/1175120971062054964/1216391019960991825/standard_6.gif?ex=6699256a&is=6697d3ea&hm=c8d97c97c45785d6e1ab8aad3c534c5d98d22fbf020b46555dc7809ae5a3a360&')
             .setTimestamp();
 
       return interaction.reply({ embeds: [embed] });
